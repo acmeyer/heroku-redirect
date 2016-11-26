ddc7683d82e3a123bc1d8162180fe958593641ca
Alex Meyer <acmeyer9@gmail.com>
2016-11-26 15:00:03 -0500

    update server code

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('*', function(request, response) {
   response.redirect(process.env.NEW_BASE_URL + request.url)
